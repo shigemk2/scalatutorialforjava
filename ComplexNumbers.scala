5ffd5200e1964a58415a2641c93513d69b2bec06
shigemk2 <i.am.shige@gmail.com>
2014-10-08 00:06:03 +0900

    5.1 引数無しのメソッド

---
+++ ComplexNumbers.scala
@@ -0,0 +1,12 @@
+// Scala はオブジェクト指向言語であり、それゆえにクラスの概念があります。Scala のクラスは、Java の構文と似た構文で宣言します。重要な違いは Scala のクラスはパラメータを持てるということ
+class Complex(real: Double, imaginary: Double) {
+  def re() = real
+  def im() = imaginary
+}
+
+object ComplexNumbers {
+  def main(args: Array[String]) {
+    val c = new Complex(1.2, 3.4)
+    println("imaginary part: " + c.im())
+  }
+}
